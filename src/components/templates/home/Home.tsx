e5a72bdde7157601659e36be4bf0b213e6a19b4e
paulo diaz <paulodiaz@MacBook-Pro-de-paulo-2.local>
2022-11-05 14:33:59 -0700

    inicial commit

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,63 +1,32 @@
-import { CheckCircleIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Heading, VStack, List, ListIcon, ListItem } from '@chakra-ui/react';
+import { VStack, Heading, Input, Text, Box, Container, Center } from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <VStack w={'full'}>
-      <Heading size="md" marginBottom={6}>
-        Ethereum Boilerplate
-      </Heading>
-      <List spacing={3}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Moralis authentication
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display Transactions
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Multichain Support
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Adding explorer links to balances, transactions ...
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Better responsive design
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Rainbowkit integration
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Using Moralis from client-side
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          ... and more
-        </ListItem>
-      </List>
-    </VStack>
+    <Center
+      width={'100%'}
+      backgroundImage="url('./Scandinavian-Home-Exterior20.png')"
+      backgroundSize={'cover'}
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      minHeight={'756px'}
+    >
+      <VStack w={'full'} spacing={20}>
+        <Heading as="h2" size="2xl" marginBottom={6} color={'#FEFEFE'}>
+          Discover new opportunities
+        </Heading>
+        <Input
+          placeholder="Search locations"
+          width={'25%'}
+          maxWidth={'320px'}
+          borderColor="#FFA500"
+          bgColor={'#FEFEFE'}
+          color={'#AEAEAE'}
+        />
+        <Text fontSize="3xl" color={'#FEFEFE'}>
+          We help people invest in property
+        </Text>
+      </VStack>
+    </Center>
   );
 };
 
